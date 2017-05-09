f19fb9438a708ff3e09abc086bfd3b3d78e4db42
axel-ft <axel.floquettrillot@ynov.com>
2017-05-09 12:40:47 +0000

    general update

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,1 @@
-var win = function() {
-    document.cookie
-}
+
--- js/init.js
+++ js/init.js
@@ -1,13 +1,11 @@
 // Define initial variables
 if (typeof(Storage) !== "undefined") {
     // Code for localStorage/sessionStorage.
-    sessionStorage.setItem("points", 20);
+    localStorage.setItem("points", 20);
 } else {
     // Sorry! No Web Storage support..
     if ((document.cookie.match(/^(?:.*;)?\s*points\s*=\s*([^;]+)(?:.*)?$/)||[,null])[1] == null ) {
         document.cookie = "points=20";
     }
 }
-
-
 
